fd6fe92208691a9a4e51841e0f16e46705db0e0a
Jagdish Butte <jagdishbutte@gmail.com>
2025-06-25 10:07:52 +0530

    feat: setup for api integration

---
+++ src/api/apis.ts
@@ -0,0 +1,6 @@
+import { BASE_URL } from "@/lib/apiConnection";
+
+export const userAuth = {
+    LOGIN: `${BASE_URL}api/auth/login`,
+    REGISTER: `${BASE_URL}api/auth/register`,
+};
---
+++ src/lib/apiConnector.ts
@@ -0,0 +1,50 @@
+"use client";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export const axiosInstance = axios.create({});
+
+export const apiConnector = async (
+    method: string,
+    url: string,
+    data: object | null,
+    headers: object | null,
+    params: object | null,
+    responseType:
+        | "arraybuffer"
+        | "blob"
+        | "json"
+        | "text"
+        | "stream"
+        | null
+) => {
+    try {
+        const fields = {
+            method: method,
+            url: url,
+            data: data || undefined,
+            headers: headers || undefined,
+            params: params || undefined,
+            responseType: responseType || undefined,
+        };
+
+        const response = await axiosInstance(fields);
+
+        return response;
+    } catch (error: unknown) {
+        if ((error as { response?: { status?: number } })?.response?.status === 408) {
+            toast.error("Session Expired! Please login again.");
+            setTimeout(() => {
+                localStorage.clear();
+                if (typeof window !== "undefined") window.location.reload();
+            }, 200);
+        }
+        // if (error.response) {
+        // //   console.error("Error response data:", error.response.data);
+        // }
+        throw error;
+
+        // const errorMessage = error?.response?.data?.error || "An error occurred";
+        // toast.error(errorMessage);
+    }
+};
